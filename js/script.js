1f0e5ac45f046d17abb06affc9f52157324b1309
brendayw <brendayw97@gmail.com>
2025-04-01 00:54:10 -0300

    demo de varias secciones

--- js/script.js
+++ js/script.js
@@ -1,16 +1,29 @@
-const flechas = document.querySelectorAll(".img-flecha img");
+document.querySelectorAll('.menu-nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
 
+        const destino = document.querySelector(this.getAttribute('href'));
+        
+        destino.scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+const flechas = document.querySelectorAll('.img-flecha img');
 flechas.forEach(flecha => {
-    flecha.addEventListener("click", function () {
-        // Encuentra la sección actual (donde está la flecha)
-        let seccionActual = flecha.closest("section");
-
-        // Encuentra la siguiente sección
+    flecha.addEventListener('click', function() {
+        let seccionActual = flecha.closest('.siguiente-seccion');
         let siguienteSeccion = seccionActual.nextElementSibling;
 
-        // Si existe una siguiente sección, desplázate suavemente
+        while (siguienteSeccion && !siguienteSeccion.classList.contains('siguiente-seccion')) {
+            siguienteSeccion = siguienteSeccion.nextElementSibling;
+        }
+
         if (siguienteSeccion) {
-            siguienteSeccion.scrollIntoView({ behavior: "smooth" });
+            siguienteSeccion.scrollIntoView({
+                behavior: 'smooth'
+            });
         }
     });
 });
